e98bf32c9619f8fd8f7727a0fa2d8929ab497058
matthias-matthies <matthias.matthies.business@gmail.com>
2022-02-07 13:16:17 +0100

    removed test

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__ . '/vendor/autoload.php';
-
-use CNTRX\Database;
-
-$database = new Database();
-$database->setCurrentGreedFearIndex();
-echo $database->getCurrentGreedFearIndex();
